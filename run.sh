93b2c92038b2a1fa50512bd21ab40c7aea5d4487
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-03 20:51:04 +0800

    fix run.sh cmds

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 repos=(
-    "git clone https://github.com/axwhyzee/multi-modal-retrieval-storage-service.git"
-    "git clone https://github.com/axwhyzee/multi-modal-retrieval-gateway-service.git"
-    "git clone https://github.com/axwhyzee/multi-modal-retrieval-embedding-service.git"
-    "git clone https://github.com/axwhyzee/multi-modal-retrieval-preprocessor-service.git"
-    "git clone https://github.com/axwhyzee/multi-modal-retrieval-frontend.git"
+    "multi-modal-retrieval-storage-service"
+    "multi-modal-retrieval-gateway-service"
+    "multi-modal-retrieval-embedding-service"
+    "multi-modal-retrieval-preprocessor-service"
+    "multi-modal-retrieval-frontend"
 )
 
 for repo in "${repos[@]}"; do
     if [ -d "$repo" ]; then
         echo "$repo already exists, skipping..."
     else
-        git clone "https://github.com/axwhyzee/$repo.git" && echo "Cloned $repo
+        git clone "https://github.com/axwhyzee/$repo.git" && echo "Cloned $repo"
     fi
 
 docker-compose up -d
+
+done
